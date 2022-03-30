2fdfe92669aeab4ea72818b3f0d78842ade5155a
Jorge Trad <jetradl@gmail.com>
2022-03-30 11:11:01 -0600

    Little fix at showing th date and hour in practice 1 Unit 2

--- pages/practice1U2.tsx
+++ pages/practice1U2.tsx
@@ -1,11 +1,18 @@
 import { Container } from "@mui/material";
 
+
+
 const Practice1U2 = () => {
+    const myDate = new Date();
+
+    const addZeroToLeft = (item) => {
+        return item < 10 ? `0${item}` : item;
+    }
     return (
         <Container>
             <h1>Testing expreessions</h1>
-            <h3>The date and hour now: {new Date().toString()}</h3>
-            <h3>Local date and hour: {new Date().toString()}</h3>
+            <h3>The date and hour now: { myDate.toString() }</h3>
+            <h3>Local date and hour: { `${myDate.toDateString()} ${ addZeroToLeft(myDate.getHours()) }:${ addZeroToLeft(myDate.getMinutes()) }:${ addZeroToLeft(myDate.getSeconds()) }` }</h3>
             <h3>16 squared: {Math.pow(16, 2)}</h3>
             <h3>Square root of 16: {Math.sqrt(16)}</h3>
             <br /><br />
